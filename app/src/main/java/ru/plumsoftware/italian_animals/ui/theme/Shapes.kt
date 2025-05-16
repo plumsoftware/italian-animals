5dc1fd25a2de88ad3a7eded32a9578424abad637
Vyacheslav Deych <plumsoftwareofficial@gmail.com>
2025-05-16 17:42:41 +0600

    Добавил тему

---
+++ app/src/main/java/ru/plumsoftware/italian_animals/ui/theme/Shapes.kt
@@ -0,0 +1,12 @@
+package ru.plumsoftware.italian_animals.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+
+val shapes = Shapes(
+    extraSmall = RoundedCornerShape(UIConstants.extraSmall),
+    small = RoundedCornerShape(UIConstants.small),
+    medium = RoundedCornerShape(UIConstants.medium),
+    large = RoundedCornerShape(UIConstants.large),
+    extraLarge = RoundedCornerShape(UIConstants.extraLarge),
+)
---
+++ app/src/main/java/ru/plumsoftware/italian_animals/ui/theme/UIConstants.kt
@@ -0,0 +1,11 @@
+package ru.plumsoftware.italian_animals.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object UIConstants {
+    val extraSmall = 2.dp
+    val small = 4.dp
+    val medium = 8.dp
+    val large = 16.dp
+    val extraLarge = 24.dp
+}
